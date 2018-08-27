d9e9b1b1a7dd3a606acdf1b51e5fb1b3e47c19aa
Fisher Yu <i@yf.io>
2018-08-27 02:34:40 -0700

    add label2det v2

---
+++ bdd_data/label2det_v2.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+
+
+def parse_args():
+    """Use argparse to get command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('label_path', help='path to the label dir')
+    parser.add_argument('det_path', help='path to output detection file')
+    args = parser.parse_args()
+
+    return args
+
+
+def label2det(frames):
+    boxes = list()
+    for frame in frames:
+        for label in frame['labels']:
+            if 'box2d' not in label:
+                continue
+            xy = label['box2d']
+            if xy['x1'] >= xy['x2'] or xy['y1'] >= xy['y2']:
+                continue
+            box = {'name': frame['name'],
+                   'timestamp': frame['timestamp'],
+                   'category': label['category'],
+                   'bbox': [xy['x1'], xy['y1'], xy['x2'], xy['y2']],
+                   'score': 1}
+            boxes.append(box)
+    return boxes
+
+
+def convert_labels(label_path, det_path):
+    frames = json.load(open(label_path, 'r'))
+    det = label2det(frames)
+    json.dump(det, open(det_path, 'w'), indent=4, separators=(',', ': '))
+
+
+def main():
+    args = parse_args()
+    convert_labels(args.label_path, args.det_path)
+
+
+if __name__ == '__main__':
+    main()
